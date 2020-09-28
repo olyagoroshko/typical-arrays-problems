82b5e81cab1f329d2ef9ec9769c8976b46882712
olyagoroshko <olga.og.goroshko@gmail.com>
2020-09-28 17:14:53 +0300

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  let i = 0;
+  if(array == null || array.length == 0){
+    return 0;
+  } else {
+    let sortArr = array.sort(function(a, b) {
+      return a - b;
+    });
+    return sortArr[i];
+  }
+};
 
 exports.max = function max (array) {
-  return 0;
+  let i = 0;
+  if(array == null || array.length == 0){
+    return 0;
+  } else {
+    let sortArr = array.sort(function(a, b) {
+      return a - b;
+    }).reverse();
+    return sortArr[i];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array == null || array.length == 0){
+    return 0;
+  } else {
+    let sum = 0;
+    for(let i = 0; i < array.length; i++){
+      sum+=array[i];
+    }
+    return sum/array.length;
+  }
 }
